c312f6171b66d83f35dfdabd006a22e6663848ed
Kira Miller <31229189+kiram15@users.noreply.github.com>
2026-03-05 19:47:25 +0000

    fix: quality fixes

--- openedx/core/djangoapps/user_api/accounts/tests/test_retirement_views.py
+++ openedx/core/djangoapps/user_api/accounts/tests/test_retirement_views.py
@@ -57,7 +57,6 @@ from openedx.core.djangoapps.credit.models import (
 )
 from openedx.core.djangoapps.external_user_ids.models import ExternalIdType
 from openedx.core.djangoapps.oauth_dispatch.jwt import create_jwt_for_user
-from openedx.core.djangoapps.site_configuration.tests.factories import SiteFactory
 from openedx.core.djangoapps.user_api.accounts.views import AccountRetirementPartnerReportView
 from openedx.core.djangoapps.user_api.models import (
     RetirementState,
