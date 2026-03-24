130b5f3545bb2efb417fba95e9762cbe97eaa1bb
salman2013 <salman.nawaz@arbisoft.com>
2026-03-25 00:06:28 +0500

    fix: remvove unused code

--- openedx/envs/common.py
+++ openedx/envs/common.py
@@ -518,7 +518,6 @@ MAKO_TEMPLATE_DIRS_BASE = [
 # Since the CMS uses the LMS's list of mako template directories for the "preview"
 # template engine, we define the list here
 lms_mako_template_dirs_base = list(MAKO_TEMPLATE_DIRS_BASE)
-lms_mako_template_dirs_base.insert(2, XMODULE_ROOT / 'capa' / 'templates')
 lms_mako_template_dirs_base.append(REPO_ROOT / 'lms' / 'djangoapps' / 'teams' / 'templates')
 lms_mako_template_dirs_base.append(OPENEDX_ROOT / 'features' / 'course_experience' / 'templates')
 
