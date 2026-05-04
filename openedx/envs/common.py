daef3f76e4ffd7f226761cda96ce31b54dc86f13
Kyle D McCormick <kyle@axim.org>
2026-05-04 15:35:47 -0400

    docs: Update references xblocks-contrib -> xblocks-core

--- openedx/envs/common.py
+++ openedx/envs/common.py
@@ -2105,7 +2105,7 @@ XBLOCK_MIXINS = (
 # .. toggle_name: USE_EXTRACTED_WORD_CLOUD_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted Word Cloud XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted Word Cloud XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
@@ -2114,7 +2114,7 @@ USE_EXTRACTED_WORD_CLOUD_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_ANNOTATABLE_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted annotatable XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted annotatable XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
@@ -2123,7 +2123,7 @@ USE_EXTRACTED_ANNOTATABLE_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_POLL_QUESTION_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted poll question XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted poll question XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
@@ -2132,7 +2132,7 @@ USE_EXTRACTED_POLL_QUESTION_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_LTI_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted LTI XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted LTI XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
@@ -2141,7 +2141,7 @@ USE_EXTRACTED_LTI_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_HTML_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted HTML XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted HTML XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
@@ -2150,7 +2150,7 @@ USE_EXTRACTED_HTML_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_DISCUSSION_BLOCK
 # .. toggle_default: False
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted Discussion XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted Discussion XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
@@ -2160,7 +2160,7 @@ USE_EXTRACTED_DISCUSSION_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_PROBLEM_BLOCK
 # .. toggle_default: False
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted Problem XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted Problem XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
@@ -2170,7 +2170,7 @@ USE_EXTRACTED_PROBLEM_BLOCK = True
 # .. toggle_name: USE_EXTRACTED_VIDEO_BLOCK
 # .. toggle_default: True
 # .. toggle_implementation: DjangoSetting
-# .. toggle_description: Enables the use of the extracted Video XBlock, which has been shifted to the 'openedx/xblocks-contrib' repo.
+# .. toggle_description: Enables the use of the extracted Video XBlock, which has been shifted to the 'openedx/xblocks-core' repo.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2026-04-10
